83ba27f8605f1cd93899b30d5ac1a282810a57cb
Chad Duffey <chadduffey@hotmail.com>
2017-10-16 22:05:03 -0700

    searching for .git folders in dropbox

--- app.py
+++ app.py
@@ -2,12 +2,14 @@ import dropbox
 import os
 
 
-token = os.environ['DB_TOKEN']
+TOKEN = os.environ['DB_TOKEN']
 
 
 def check_dropbox_sdk_init(db_object):
+	""" Quick check to make sure we successfully initialized a Dropbox object. 
+	"""
 	try:
-		basic_info = dbx.users_get_current_account()
+		db_object.users_get_current_account()
 		print("[*] Success. Connected to Dropbox API")
 		return True
 	except:
@@ -17,22 +19,42 @@ def check_dropbox_sdk_init(db_object):
 	return False
 
 
-def list_dropbox_folder(dbx, path):
-
+def list_dropbox_folders(dbx, path):
+	""" List all the Dropbox folders
+	"""
 	try:
 		dir_listing = dbx.files_list_folder(path)
 
 		for item in dir_listing.entries:
 			
 			if type(item) == dropbox.files.FileMetadata: 
-				print("*{}".format(item.name))
+				print(". {}".format(item.name))
 
 			if type(item) == dropbox.files.FolderMetadata: 
 				print("") 
 				print("{}".format(item.path_display))
-				list_dropbox_folder(dbx, item.path_display)
-	except:
+				list_dropbox_folders(dbx, item.path_display)
+	except:		
+		if path == "":
+			path = "{folder root}"
 		
+		print("[!] Failed to return path {}".format(path))
+
+
+def find_dot_git_folders(dbx, path, delete=False):
+	""" Find, and optionally delete the .git folders in Dropbox
+	"""
+	try:
+		dir_listing = dbx.files_list_folder(path)
+
+		for item in dir_listing.entries:
+
+			if type(item) == dropbox.files.FolderMetadata: 
+				if item.name == ".git":
+					print("") 
+					print("{}".format(item.path_display))
+				find_dot_git_folders(dbx, item.path_display)
+	except:		
 		if path == "":
 			path = "{folder root}"
 		
@@ -41,9 +63,10 @@ def list_dropbox_folder(dbx, path):
 
 if __name__ == "__main__":
 
-	dbx = dropbox.Dropbox(token)
+	dbx = dropbox.Dropbox(TOKEN)
 	check_dropbox_sdk_init(dbx)
-	list_dropbox_folder(dbx, "")
+	#list_dropbox_folders(dbx, "")
+	find_dot_git_folders(dbx, "")
 
 
 
