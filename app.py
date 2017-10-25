26be9902cb62d44e99c7353d9e257b1f12663778
Chad Duffey <chad@chad-mbp.corp.dropbox.com>
2017-10-25 16:47:34 -0700

    added bootstrap, tidied debug info

--- app.py
+++ app.py
@@ -4,13 +4,17 @@ import os
 import dbcheck
 import stats
 
-from flask import Flask, render_template 	
+from flask import Flask, render_template 
+from flask_bootstrap import Bootstrap	
 
 TOKEN = os.environ['DB_TOKEN']
 DEBUG = True
 
-
 app = Flask(__name__)
+Bootstrap(app)
+
+app.config['SECRET_KEY'] = 'secret!'
+app.config['DEBUG'] = DEBUG
 
 
 @app.route('/')
@@ -28,7 +32,7 @@ if __name__ == "__main__":
 
 	dbx = dropbox.Dropbox(TOKEN)
 	connection_result = dbcheck.check(dbx)
-	stats_dict, size_dict = stats.dropbox_stats(dbx, "", testing_mode=True)
+	stats_dict, size_dict = stats.dropbox_stats(dbx, "", testing_mode=DEBUG)
 
 	app.run()
 
